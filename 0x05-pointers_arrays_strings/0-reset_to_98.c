7a904a0a4d8f52332e7f92664ec2bca1832efc25
EzeMatthew <ezebongmathew5@gmail.com>
2023-02-27 13:45:54 -0800

    0-98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,8 @@
 
 /**
  * reset_to_98 - a function that takes a pointer to an int as parameter
- * and update the value it points to to 98
- * @n;input
+ * and updates the value it points to to 98
+ * @n: input
  * Return: n
  */
 
